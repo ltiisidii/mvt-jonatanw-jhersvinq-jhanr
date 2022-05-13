8dd05419321bfede361872dbd65d573e55599ca6
ltiisidii <jonatan.wb@gmail.com>
2022-05-12 21:38:35 -0300

    primer commit

---
+++ appfamiliares/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Familiar(models.Model):
+    
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    relacion = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    domicilio = models.CharField(max_length=50)
+    fecha_creacion = models.DateField(auto_now_add=True, verbose_name="Fecha de creación")
---
+++ appfamiliares/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.template import loader
+from django.template import Template, Context
+from django.http import HttpResponse
+from appfamiliares.models import Familiar
+# Create your views here.
+
+def mis_familiares(request):
+    template = loader.get_template('index.html')
+    documento = template.render({
+        'familiares': Familiar.objects.all()
+    })
+    return HttpResponse(documento)
